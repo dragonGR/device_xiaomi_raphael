976b36b782962d4260e59ba504f779a21c3cccc1
dragonGR <alex@dragongr.dev>
2020-11-23 12:57:35 +0200

    AICPfy it

--- aicp_raphael.mk
+++ aicp_raphael.mk
@@ -6,14 +6,17 @@
 
 $(call inherit-product, device/xiaomi/raphael/device.mk)
 
-# Inherit some common Lineage stuff.
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+# Inherit some common aicp stuff.
+$(call inherit-product, vendor/aicp/config/common_full_phone.mk)
 
 # Device identifier. This must come after all inclusions.
 PRODUCT_BRAND := Xiaomi
 PRODUCT_DEVICE := raphael
 PRODUCT_MANUFACTURER := Xiaomi
-PRODUCT_NAME := lineage_raphael
+PRODUCT_NAME := aicp_raphael
+
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    DEVICE_MAINTAINERS="Alex T. (GreekDragon), Diego Casasnovas (diego-cr)"
 
 BUILD_FINGERPRINT := "Xiaomi/raphaelin/raphaelin:9/PKQ1.181121.001/V10.3.3.0.PFKINXM:user/release-keys"
 
